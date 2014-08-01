177d5d6f05a4a4db8ad3ef5a0f2a59a9fdbb8aac
oholubyev <oholubyev@heliostech.hk>
2014-08-01 16:26:24 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FyberDeveloperChallenge::Application.config.secret_token = '2fe76cc5c744026c61cf021c4fa703e9685540aa22df3263a1da90c6d0f32b0dfb227faf6a7570c356152badebd7ab5734c8173d9f18cf8738771dbd1de9c9ae'
